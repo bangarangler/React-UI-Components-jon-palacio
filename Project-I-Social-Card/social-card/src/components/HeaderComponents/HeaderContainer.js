f1ede24743212c250e3e0a19f10216b736b53c70
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-02-11 18:57:48 -0500

    Completed MVP and learned some styled-components from Shawn

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+
+function HeaderContainer() {
+  return (
+    <div className="container">
+      <ImageThumbnail />
+      <div className="flex">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+function HeaderContent() {
+  return (
+    <p>
+      Lets's learn React by building simple interfaces wit hcomponents. Don't
+      try to overthink it, just keep it simple and have fun. Once you feel
+      comfortable using components you are well on your way to mastering React!
+    </p>
+  );
+}
+
+export default HeaderContent;
